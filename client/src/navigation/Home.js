193d316995d1a9046c3a603cebaa12ebbbd6f1b1
daryledevs <daryledevs@gmail.com>
2022-10-16 03:21:59 +0800

    Product page added to the navigation route

--- client/src/navigation/Home.js
+++ client/src/navigation/Home.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { createStackNavigator } from '@react-navigation/stack';
 import ProductScreen from '../user/page/Product';
-
+import ProductDetail from '../user/component/ProductDetail';
 const Stack = createStackNavigator();
 
 const HomeNavigator = () => {
@@ -11,6 +11,7 @@ const HomeNavigator = () => {
         headerShown: false,
       }}>
       <Stack.Screen name="Homepage" component={ProductScreen} />
+      <Stack.Screen name="Product Detail" component={ProductDetail} />
     </Stack.Navigator>
   );
 }
