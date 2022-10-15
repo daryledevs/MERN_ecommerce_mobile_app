4bd19070e81a190a11e9df73448ca8870c845cf1
daryledevs <daryledevs@gmail.com>
2022-10-16 04:33:50 +0800

    Search feature now includes a search list

---
+++ client/src/user/component/SearchedProduct.js
@@ -0,0 +1,48 @@
+import {useNavigation} from '@react-navigation/native';
+import React from 'react';
+import {StyleSheet, Text, View, Image, TouchableOpacity} from 'react-native';
+import placeholder from '../../asset/image/placeholder.png'
+const SearchedProducts = props => {
+  const { item } = props;
+  const navigation = useNavigation();
+
+  return (
+    <TouchableOpacity
+      style={styles.container}
+      onPress={() => navigation.navigate('Product Detail', {item: item})}>
+      <Image
+        style={styles.image}
+        source={placeholder}
+      />
+      <View>
+        <Text style={styles.name}>{item.name}</Text>
+        <Text style={styles.name}>{item.description}</Text>
+      </View>
+    </TouchableOpacity>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    borderBottomWidth: 1,
+    borderBottomColor: '#ccc',
+    marginBottom: 5,
+    flexDirection: 'row',
+    alignItems: 'center',
+    paddingBottom: 5,
+    paddingTop: 5,
+    paddingLeft: 5,
+  },
+
+  name: {
+    fontSize: 17,
+    paddingLeft: 5,
+  },
+
+  image: {
+    width: 40,
+    height: 40,
+  },
+});
+
+export default SearchedProducts;
