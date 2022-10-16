50cf04d68803a771fd811e59f08261c0956be15c
daryledevs <daryledevs@gmail.com>
2022-10-16 08:49:07 +0800

    Top navigator has been added including shipping, payment and confirm component

--- client/src/user/component/CartCheckout.js
+++ client/src/user/component/CartCheckout.js
@@ -1,12 +1,20 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import { createMaterialTopTabNavigator } from '@react-navigation/material-top-tabs';
+import Shipping from './Shipping';
+import Payment from './Payment';
+import Confirm from './Confirm';
+
+const Tab = createMaterialTopTabNavigator();
 
 const CartCheckout = () => {
   return (
-    <View>
-      <Text>CartCheckout</Text>
-    </View>
-  )
+    <Tab.Navigator>
+      <Tab.Screen name="Shipping" component={Shipping} />
+      <Tab.Screen name="Payment" component={Payment} />
+      <Tab.Screen name="Confirm" component={Confirm} />
+    </Tab.Navigator>
+  );
 }
 
 export default CartCheckout
