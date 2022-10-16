602754accfcd023169a4250aea50fad12b3d22c4
daryledevs <daryledevs@gmail.com>
2022-10-16 08:29:12 +0800

    Created a stack navigator for the cart

---
+++ client/src/navigation/Cart.js
@@ -0,0 +1,23 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+import { createStackNavigator } from '@react-navigation/stack'
+import CartScreen from '../user/page/Cart';
+import CartCheckout from '../user/component/CartCheckout';
+
+const Stack = createStackNavigator();
+
+const CartNavigator = () => {
+  return (
+    <Stack.Navigator
+      screenOptions={{
+        headerShown: false,
+      }}>
+      <Stack.Screen name="Cart Screen" component={CartScreen} />
+      <Stack.Screen name="Cart Checkout" component={CartCheckout} />
+    </Stack.Navigator>
+  );
+}
+
+export default CartNavigator;
+
+const styles = StyleSheet.create({})
