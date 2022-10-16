c9f072318e396b49e7c1aa7105bfe78847adbbdf
daryledevs <daryledevs@gmail.com>
2022-10-16 08:25:48 +0800

    Create a cart checkout page without a layout

---
+++ client/src/user/component/CartCheckout.js
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const CartCheckout = () => {
+  return (
+    <View>
+      <Text>CartCheckout</Text>
+    </View>
+  )
+}
+
+export default CartCheckout
+
+const styles = StyleSheet.create({})
