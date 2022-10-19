069fa407f4b1b4434887307aada80d781d37f2de
daryledevs <daryledevs@gmail.com>
2022-10-20 06:50:38 +0800

    Product schema has been created

---
+++ server/src/model/Product.js
@@ -0,0 +1,77 @@
+const mongoose = require("mongoose");
+
+const productSchema = mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+
+  description: {
+    type: String,
+    default: "",
+  },
+
+  richDescription: {
+    type: String,
+    default: "",
+  },
+
+  image: {
+    type: String,
+    default: "",
+  },
+
+  images: [
+    {
+      type: String,
+    },
+  ],
+
+  brand: {
+    type: String,
+    default: "",
+  },
+
+  price: {
+    type: Number,
+    default: 0,
+  },
+
+  category: {
+    // need to pass the id always
+    type: mongoose.Schema.Types.ObjectId,
+    // To connect it to category schema
+    ref: "Category",
+    required: true,
+  },
+
+  countInStock: {
+    type: Number,
+    required: true,
+    min: 0,
+    max: 255,
+  },
+
+  rating: {
+    type: Number,
+    default: 0,
+  },
+
+  numReviews: {
+    type: Number,
+    default: 0,
+  },
+
+  isFeatured: {
+    // to show in the homepage as feature home products
+    type: Boolean,
+    default: false,
+  },
+
+  dateCreated: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+exports.Product = mongoose.model("Product", productSchema);
