ab32f2af145fb67c66d01d30fc4f194afaeb5d90
daryledevs <daryledevs@gmail.com>
2022-10-20 09:21:56 +0800

    Created a user model schema

---
+++ server/src/model/User.js
@@ -0,0 +1,57 @@
+const mongoose = require("mongoose");
+
+const userSchema = mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+  },
+  passwordHash: {
+    type: String,
+    required: true,
+  },
+  phone: {
+    type: String,
+    required: true,
+  },
+  isAdmin: {
+    type: Boolean,
+    default: false,
+  },
+  house_number: {
+    type: Number,
+    required: true,
+  },
+
+  street: {
+    type: String,
+    required: true,
+  },
+
+  district: {
+    type: String,
+    required: true,
+  },
+
+  city: {
+    type: String,
+    default: "",
+  },
+  
+  zip: {
+    type: String,
+    default: "",
+  },
+});
+
+userSchema.virtual("id").get(function () {
+  return this._id.toHexString();
+})
+.set("toJSON", {
+  virtuals: true,
+});
+
+exports.User = mongoose.model("User", userSchema);
