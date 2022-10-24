c1b173bf583f44c97118d19d578cf02d41059795
daryledevs <daryledevs@gmail.com>
2022-10-24 09:28:25 +0800

    Implemented the authentication for login with a token

--- client/src/navigation/MainNavigation.js
+++ client/src/navigation/MainNavigation.js
@@ -1,80 +1,121 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { StyleSheet, View, Text } from 'react-native';
 // packages
 import Icon from 'react-native-vector-icons/FontAwesome5';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-import { useSelector } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 // components
-import HomeNavigator from './Home';
-import CartNavigator from './Cart';
+import { Token, Loading, LoginTriggers, NoTokenLoading, tokenIsNull } from '../redux/reducer/User';
+import AuthNavigator from './AuthNavigator';
+import { getUserInfoByToken } from '../redux/action/User';
 import UserProfile from '../user/page/UserProfile';
 import { cartData } from '../redux/reducer/Cart';
+import HomeNavigator from './Home';
+import CartNavigator from './Cart';
 
 const Tab = createBottomTabNavigator();
 
 const MainNavigation = () => {
   const cart = useSelector(cartData);
+  const isToken = useSelector(Token);
+  const isLoading = useSelector(Loading);
+  const loginTriggers = useSelector(LoginTriggers);
+  const dispatch = useDispatch();
+
+  
+   useEffect(() => {
+    if (loginTriggers) {
+      console.log('loginTriggers: ', loginTriggers);
+      dispatch(NoTokenLoading());
+    }
+
+    setTimeout(function () {
+      AsyncStorage.getItem('token')
+        .then(token => {
+          console.log('AsyncStorage (token): ', token);
+          console.log('Is token not null? ', token !== null);
+          if (token) dispatch(getUserInfoByToken());
+          else dispatch(tokenIsNull());
+        })
+        .catch(error => {
+          console.log('UserNavigator, error: ', error);
+        });
+    }, 3000);
+  }, [loginTriggers]);
+
+  if(isLoading) {
+    console.log("isLoading: ", isLoading);
+    return <Text>Loading ...</Text>;
+  }
+
   return (
-    <Tab.Navigator
-      initialRouteName="Home"
-      screenOptions={{
-        KeyboardHidesTabBar: true,
-        showLabel: false,
-        activeTinColor: '#e91e63',
-        headerShown: false,
-        tabBarStyle: {height: 60},
-        tabBarLabelStyle: {
-          fontSize: 13,
-        },
-      }}>
-      <Tab.Screen
-        name="Home"
-        component={HomeNavigator}
-        options={{
-          tabBarIcon: ({color}) => (
-            <Icon
-              name="home"
-              style={{position: 'relative'}}
-              color={color}
-              size={24}
-            />
-          ),
-        }}
-      />
-      <Tab.Screen
-        name="Cart"
-        component={CartNavigator}
-        options={{
-          tabBarIcon: ({color}) => (
-            <View>
-              <Icon
-                name="shopping-cart"
-                style={{position: 'relative'}}
-                color={color}
-                size={24}
-              />
-              {cart.length !== 0 ? (
-                <Text style={styles.countCartItems}>{cart.length}</Text>
-              ) : null}
-            </View>
-          ),
-        }}
-      />
-      <Tab.Screen
-        name="Me"
-        component={UserProfile}
-        options={{
-          tabBarIcon: ({color}) => (
-            <Icon
-              name="user-alt"
-              style={{position: 'relative'}}
-              color={color}
-              size={24}
-            />
-          ),
-        }}
-      />
-    </Tab.Navigator>
+    <>
+      {isToken ? (
+        <Tab.Navigator
+          initialRouteName="Home"
+          screenOptions={{
+            KeyboardHidesTabBar: true,
+            showLabel: false,
+            activeTinColor: '#e91e63',
+            headerShown: false,
+            tabBarStyle: {height: 60},
+            tabBarLabelStyle: {
+              fontSize: 13,
+            },
+          }}>
+          <Tab.Screen
+            name="Home"
+            component={HomeNavigator}
+            options={{
+              tabBarIcon: ({color}) => (
+                <Icon
+                  name="home"
+                  style={{position: 'relative'}}
+                  color={color}
+                  size={24}
+                />
+              ),
+            }}
+          />
+          <Tab.Screen
+            name="Cart"
+            component={CartNavigator}
+            options={{
+              tabBarIcon: ({color}) => (
+                <View>
+                  <Icon
+                    name="shopping-cart"
+                    style={{position: 'relative'}}
+                    color={color}
+                    size={24}
+                  />
+                  {cart.length !== 0 ? (
+                    <Text style={styles.countCartItems}>{cart.length}</Text>
+                  ) : null}
+                </View>
+              ),
+            }}
+          />
+          <Tab.Screen
+            name="Me"
+            component={UserProfile}
+            options={{
+              tabBarIcon: ({color}) => (
+                <Icon
+                  name="user-alt"
+                  style={{position: 'relative'}}
+                  color={color}
+                  size={24}
+                />
+              ),
+            }}
+          />
+        </Tab.Navigator>
+      ) : (
+        <AuthNavigator />
+      )}
+    </>
   );
 };
 
