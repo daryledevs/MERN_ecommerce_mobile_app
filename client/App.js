8a7ee0ccc17fd8e23358739548f498d15e69da14
daryledevs <daryledevs@gmail.com>
2022-10-24 09:54:03 +0800

    Created a loading screen for first boot of the application
    Deleted the header component
    
    Deleted the header component

--- client/App.js
+++ client/App.js
@@ -2,7 +2,6 @@ import { StyleSheet, Text, View } from 'react-native';
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 // component
-import Header from './src/shared/Header';
 import MainNavigation from './src/navigation/MainNavigation';
 import { Provider } from 'react-redux';
 import store from './src/redux/store';
@@ -11,7 +10,6 @@ const App = () => {
   return (
     <Provider store={store}>
       <NavigationContainer>
-        <Header />
         <MainNavigation />
       </NavigationContainer>
     </Provider>
---
+++ client/src/shared/FirstBootLoading.js
@@ -0,0 +1,25 @@
+import { StyleSheet, Text, View, Image } from 'react-native'
+import React from 'react'
+import logo from '../asset/image/logo.png'
+const FirstBootLoading = () => {
+  return (
+    <View style={styles.container}>
+      <Image source={logo} style={styles.image} />
+    </View>
+  )
+}
+
+export default FirstBootLoading
+
+const styles = StyleSheet.create({
+  container: {
+    justifyContent: 'center',
+    alignSelf: 'center',
+    flex: 1,
+  },
+
+  image: {
+    width: 200,
+    height: 200,
+  },
+});
