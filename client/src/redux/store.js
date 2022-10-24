871eee95edf345057b61b1a43665c97e7749b81e
daryledevs <daryledevs@gmail.com>
2022-10-24 11:12:52 +0800

    Update store for product reducer

--- client/src/redux/store.js
+++ client/src/redux/store.js
@@ -1,11 +1,12 @@
-import { configureStore, getDefaultMiddleware } from "@reduxjs/toolkit";
+import { configureStore } from "@reduxjs/toolkit";
 import cartReducer from "./reducer/Cart";
 import userReducer from './reducer/User';
-
+import productReducer from "./reducer/Product";
 const store = configureStore({
   reducer: {
     cart: cartReducer,
     user: userReducer,
+    product: productReducer,
   },
 });
 
