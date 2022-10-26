db6cab25aec6b3c8fc1c435d2575413f9906da4d
daryledevs <daryledevs@gmail.com>
2022-10-26 16:27:47 +0800

    Created an async thunk getAllCategories action

--- client/src/redux/action/Product.js
+++ client/src/redux/action/Product.js
@@ -2,16 +2,37 @@ import { createAsyncThunk } from "@reduxjs/toolkit";
 import api from "../../asset/api";
 
 
-export const getAllProducts = createAsyncThunk("product/getAllProducts", async() => {
-  const promise = api.get("/products")
-    .then((response) => {
-      console.log("Products: ", response.data);
-      return response.data;
-    })
-    .catch((error) => {
-      console.log(error);
-    });
+export const getAllProducts = createAsyncThunk(
+  'product/getAllProducts',
+  async () => {
+    const promise = api
+      .get('/products')
+      .then(response => {
+        console.log('Products: ', response.data);
+        return response.data;
+      })
+      .catch(error => {
+        console.log(error);
+      });
 
-  const data = await promise;
-  return data;
-})
+    const data = await promise;
+    return data;
+  },
+);
+
+export const getAllCategories = createAsyncThunk(
+  'category/getAllCategories',
+  async () => {
+    const promise = api
+      .get('/categories')
+      .then(response => {
+        return response.data;
+      })
+      .catch(error => {
+        console.log(error);
+      });
+    const data = await promise;
+    console.log("Categories: ", data)
+    return data;
+  },
+);
