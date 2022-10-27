c24046465d168a00febdde8fe0fba2a3f733e9ab
daryledevs <daryledevs@gmail.com>
2022-10-27 17:26:41 +0800

    Added a quantity condition to the function

--- client/src/redux/action/Cart.js
+++ client/src/redux/action/Cart.js
@@ -1,6 +1,16 @@
 export const addToCartAction = (state, action) => {
   // state.push(action.payload)
-  return [ ...state, action.payload ];
+  if(state.some(element => element._id === action.payload._id)){
+    let allState = [...state];
+    let itemCart = state.find(item => item._id === action.payload._id);
+    const index = state.indexOf(itemCart);
+    itemCart.quantity += 1;
+    allState[index] = itemCart;
+    state = [...allState]
+    console.log(state[index]);
+  } else {
+    return [...state, { ...action.payload, quantity: 0 }];
+  }
 };
 
 export const removeFromCartAction = (state, action) => {
