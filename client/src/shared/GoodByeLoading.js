2638202fb44e149c692b1f1ff69332a26a50c2ea
daryledevs <daryledevs@gmail.com>
2022-10-30 15:37:39 +0800

    Created a new loading screen when a user logs out

---
+++ client/src/shared/GoodByeLoading.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import sad from '../asset/image/sad.png';
+import { StyleSheet, Text, View, Image, Modal } from 'react-native';
+
+const GoodByeLoading = ({ isVisible }) => {
+  return (
+    <Modal visible={isVisible}>
+      <View style={styles.container}>
+        <Image source={sad} style={styles.image} />
+        <Text style={styles.goodbyeText}>Goodbye</Text>
+      </View>
+    </Modal>
+  );
+};
+
+export default GoodByeLoading;
+
+const styles = StyleSheet.create({
+  container: {
+    justifyContent: 'center',
+    alignSelf: 'center',
+    alignItems: 'center',
+    flex: 1,
+  },
+
+  image: {
+    width: 140,
+    height: 140,
+  },
+
+  goodbyeText:{
+    fontWeight: 'bold',
+    fontSize: 20
+  }
+});
