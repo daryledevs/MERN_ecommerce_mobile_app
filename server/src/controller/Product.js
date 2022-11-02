2c9e618e4a7ddd6d9d26e1bad6da526539eaf5ff
daryledevs <daryledevs@gmail.com>
2022-11-02 14:59:54 +0800

    Configured createProduct for multer function for uploading image

--- server/src/controller/Product.js
+++ server/src/controller/Product.js
@@ -11,19 +11,28 @@ const createProduct = async (req, res) => {
   const body = req.body;
   const categoryId = req.body.category;
   const isCategoryExist = await Category.findById(categoryId);
-  
-  if(!isCategoryExist) return res.status(404).json({ success: false, message: "Invalid category." })
+  const filename = req.file.filename;
+  // req.protocol is "http/https", req.get("host") is simply the localhost port (i.e "5000")
+  const basePath = `${req.protocol}://${req.get("host")}/uploads/product`;
+
+  if (!isCategoryExist) return res.status(404).json({ success: false, message: "Invalid category." });
   if (!body) return res.status(500).json({ success: false });
 
-  const newProduct = new Product({ ...body });
-  newProduct.save()
+  const newProduct = new Product({
+    ...body,
+    // We need to specify this for the frontend to know where this can be retrieved
+    image: `${basePath}${filename}`,
+  });
+
+  newProduct
+    .save()
     .then((createdProduct) => {
-      if(!createdProduct) return res.status(500).json({ success: false });
+      if (!createdProduct) return res.status(500).json({ success: false });
       res.send(createdProduct);
     })
     .catch((error) => {
-      res.status(500).json({ success: false, message: error})
-    })
+      res.status(500).json({ success: false, message: error });
+    });
 }
 
 module.exports = { getAllProducts, createProduct };
