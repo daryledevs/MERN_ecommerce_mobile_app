6af81f27b899566530e695bb8859680387f48fd7
daryledevs <daryledevs@gmail.com>
2022-11-04 05:40:32 +0800

    Changed middleware multer's options for sending images and new endpoint for update product

--- server/src/route/Product.js
+++ server/src/route/Product.js
@@ -1,12 +1,28 @@
 const express = require("express");
 const router = express.Router();
-const { getAllProducts, createProduct } = require("../controller/Product");
+const {
+  getAllProducts,
+  createProduct,
+  updateProduct,
+} = require("../controller/Product");
 const uploadImage = require("../helper/multer");
 
 const uploadOption = uploadImage("./src/uploads/product");
 
 router.get("/", getAllProducts);
-router.post("/", uploadOption.single("image"), createProduct);
 
+router.post("/", uploadOption.fields([
+  { name: "image", maxCount: 1 },
+  { name: "images", maxCount: 7 }
+]), createProduct);
+
+router.put(
+  "/update/:id",
+  uploadOption.fields([
+    { name: "image", maxCount: 1 },
+    { name: "images", maxCount: 7 },
+  ]),
+  updateProduct
+);
 
 module.exports = router;
