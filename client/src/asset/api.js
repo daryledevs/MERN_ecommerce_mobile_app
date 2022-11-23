9763f2a46e0057c42215a34e7cc8d732e6546372
daryledevs <daryledevs@gmail.com>
2022-11-23 12:05:18 +0800

    Changed the base url for iOS

--- client/src/asset/api.js
+++ client/src/asset/api.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 import { Platform } from 'react-native';
 
 const api = axios.create({
-  baseURL: Platform.OS === "android" ? 'http://10.0.2.2:5000/api/v1' : 'http://localhost:5000'
+  baseURL: Platform.OS === "android" ? 'http://10.0.2.2:5000/api/v1' : 'http://localhost:6000/api/v1'
 });
 
 export default api;
