3411ba53087f43cfd1114786ce13c3cecacc6905
daryledevs <daryledevs@gmail.com>
2022-11-25 07:00:19 +0800

    Fixed the error 'Sending onAnimatedValueUpdate with no listeners registered'

--- client/src/util/ResetHistory.js
+++ client/src/util/ResetHistory.js
@@ -3,26 +3,26 @@ import {
   CommonActions,
   useNavigation,
 } from '@react-navigation/native';
-import { StyleSheet } from 'react-native'
-import React from 'react'
+import {StyleSheet} from 'react-native';
+import React, { useCallback } from 'react';
 
 const ResetHistory = (routeName) => {
   const navigation = useNavigation();
 
-  useFocusEffect(
-    React.useCallback(() => {
-      return () => {
-        navigation.dispatch(state => {
-          return CommonActions.reset({
-            index: 0,
-            routes: [{name: `${routeName}`}],
-          });
+  const clearNavigationHistory = useCallback(() => {
+    return () => {
+      navigation.dispatch(state => {
+        return CommonActions.reset({
+          index: 0,
+          routes: [{ name: `${routeName}` }],
         });
-      };
-    }, []),
-  );
-}
+      });
+    };
+  }, [routeName]);
 
-export default ResetHistory
+  useFocusEffect(clearNavigationHistory);
+};
 
-const styles = StyleSheet.create({})
+export default ResetHistory;
+
+const styles = StyleSheet.create({});
