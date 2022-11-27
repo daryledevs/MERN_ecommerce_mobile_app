d1f428356674889febf41f48d0ccd09003b83a80
daryledevs <daryledevs@gmail.com>
2022-11-27 13:22:25 +0800

    Refactored the getUserByToken and added correct status to each response

--- server/src/controller/User.js
+++ server/src/controller/User.js
@@ -4,39 +4,33 @@ const jwt = require("jsonwebtoken");
 
 const getAllUser = async (req, res) => {
   const userList = await User.find();
-  
-  if (!userList) return res.status(500).json({ success: false, message: "No users found." });
-  res.status(200).send(userList);
+
+  if (!userList) return res.status(404).send("No users found");
+  res.send(userList);
 };
 
 const getUserByToken = async (req, res) => {
   const secret = process.env.JWT_SECRET;
   const token = req.headers.authorization.replace("Bearer ", "");
-  const { userId, isAdmin, error } =
-    jwt.verify(token, secret, (error, decoded) => {
-      if (error?.name) {
-        res.status(404).send("Token is expired.");
-        return;
-      }
-      return decoded;
-    }) || { error: "jwt expired" }; // solve to error if destructuring the property is undefined
   
-  if(error === "jwt expired") return;
-  if (!userId) return res.status(404).send("User ID not found.");
+  jwt.verify(token, secret, async (error, decode) => {
+    if (error) return res.status(401).send("Token is expired");
 
-  const userList = await User.findById(userId);
-  res.status(200).send(userList);
+    const { userId, isAdmin } = decode;
+    const userList = await User.findById(userId);
+
+    if (!userList) return res.status(404).send("User not found");
+    res.status(200).send(userList);
+  });
 };
 
 const getNumberOfUser = async (req, res) => {
   const userCount = await User.estimatedDocumentCount();
 
   if (!userCount) {
-    res.status(500).json({ success: false });
+    res.status(404).send("No count for number of user");
   }
-  res.send({
-    userCount: userCount,
-  });
+  res.status(200).send({ userCount: userCount });
 };
 
 const createUser = async (req, res) => {
@@ -46,43 +40,41 @@ const createUser = async (req, res) => {
   const newUser = new User({
     ...body,
     passwordHash: bcrypt.hashSync(userPassword, 10),
-    creation_time: Date.now()
+    creation_time: Date.now(),
   });
 
   newUser
     .save()
     .then((createdUser) => {
-      res.status(200).send(createdUser);
+      res.send(createdUser);
     })
     .catch((error) => {
-      res.status(500).json({ success: false, error });
+      res.status(500).send(error.message);
     });
 };
 
 const userLogIn = async (req, res) => {
   const { email, password } = req.body;
-  const userDetails = await User.findOne({ email });
+  const userDetails = await User.findOne({ email: email.toLowerCase() });
   const secret = process.env.JWT_SECRET;
-
-  if(!userDetails) return res.status(404).json({ success: false, message: "user not found."});
+  if (!userDetails) return res.status(404).send("User not found");
 
   try {
-    if(await bcrypt.compare(password, userDetails.passwordHash)){
+    if (await bcrypt.compare(password, userDetails.passwordHash)) {
       const token = jwt.sign(
         { userId: userDetails._id, isAdmin: userDetails.isAdmin },
         secret,
-        { expiresIn: "1d"}
+        { expiresIn: "1d" }
       );
 
-      res.status(200).send({ email: email, token: token })
-
+      res.send({ email: email, token: token });
     } else {
-      res.status(500).send("Invalid email or password.");
+      return res.status(401).send("Invalid email or password");
     }
   } catch (error) {
-    res.status(500).json({ success: false, error});
+    res.status(500).send(error.message);
   }
-}
+};
 
 const userLogout = async (req, res) => {
   const { id } = req.params;
@@ -90,40 +82,45 @@ const userLogout = async (req, res) => {
     { _id: id },
     { last_time_sign_in: Date.now() }
   );
-  if(!user) return res.status(404).send("Something went wrong.");
+  if (!user) return res.send("Something went wrong");
 
-  res.status(200).send("Logout successfully.");
-}
+  res.send("Logout successfully");
+};
 
 const userResetPassword = async (req, res) => {
   const { email, old_password, new_password, confirm_password } = req.body;
 
   const user = await User.findOne({ email });
-  if(!user) return res.status(500).send("Incorrect email.");
+  if (!user) return res.status(401).send("Incorrect email");
 
   const compare = await bcrypt.compare(old_password, user.passwordHash);
-  if(!compare) return res.status(500).send("Incorrect old password");
+  if (!compare) return res.status(401).send("Incorrect old password");
 
-  if(new_password !== confirm_password) return res.status(500).send("New and confirm password does not match.");
+  if (new_password !== confirm_password)
+    return res.status(401).send("New and confirm password does not match");
 
   const updated_password = await User.findOneAndUpdate(
     { _id: user._id },
     { $set: { passwordHash: bcrypt.hashSync(new_password, 10) } }
   );
-  
-  if(!updated_password) return res.status(500).send("Something went wrong.");
 
-  res.status(200).json({ success: true, message: "The password has successfully changed."});
-}
+  if (!updated_password) return res.send("Something went wrong");
+
+  res
+    .status(200)
+    .send({ success: true, message: "The password has successfully changed" });
+};
 
 const deleteAccount = async (req, res) => {
   const { id } = req.params;
-  
+
   const userAccount = await User.findByIdAndDelete(id);
 
-  if(!userAccount) return res.status(404).json("User not found.");
-  res.status(200).json({ successful: true, message: "Account has been deleted."  });
-}
+  if (!userAccount) return res.status(404).send("User not found");
+  res
+    .status(200)
+    .send({ successful: true, message: "Account has been deleted" });
+};
 
 module.exports = {
   getAllUser,
