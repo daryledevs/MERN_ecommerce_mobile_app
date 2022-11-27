842c9f2eed11e449f50065336a23833dd6a39d43
daryledevs <daryledevs@gmail.com>
2022-11-27 13:13:51 +0800

    Made the userLogin to be a thunk function

--- client/src/redux/action/User.js
+++ client/src/redux/action/User.js
@@ -1,16 +1,42 @@
 import { createAsyncThunk } from "@reduxjs/toolkit";
 import AsyncStorage from '@react-native-async-storage/async-storage';
+import { getAllProducts, getAllCategories } from "../action/Product";
 import api from '../../asset/api';
+import { getUserLikes } from "./Wishlist";
 
 export const userLogin = createAsyncThunk(
   'user/userLogin',
-  async ({ email, password }, { fulfillWithValue, rejectWithValue }) => {
+  async ({ email, password }, { fulfillWithValue, rejectWithValue, dispatch }) => {
     try {
       const response = await api.post('users/login', { email, password })
-      await AsyncStorage.setItem("token", response.data.token);
+      dispatch(getUserInfoByToken(response.data.token));
+      await AsyncStorage.setItem('token', response.data.token);
       return fulfillWithValue(response.data);
     } catch (error) {
-      return rejectWithValue(error.message);
+      return rejectWithValue(error.response.data);
+    }
+  },
+);
+
+export const getUserInfoByToken = createAsyncThunk(
+  'user/getUserInfoByToken',
+  async (token, { dispatch, rejectWithValue }) => {
+    try {
+      const response = await api.get(`users/token`, {
+        headers: {
+          Authorization: `Bearer ${token}`,
+          Accept: 'application/json',
+        },
+      });
+
+      if(!response) throw new Error("Token is not verified");
+      
+      dispatch(getAllProducts());
+      dispatch(getAllCategories());
+      dispatch(getUserLikes(response.data._id));
+      return response.data;
+    } catch (error) {
+      return rejectWithValue(error.response.data);
     }
   },
 );
@@ -35,36 +61,17 @@ export const userLogoutAction = (state, action) => {
   };
   state.goodByeLoading = true;
   state.loginTriggers = false;
-  
+  state.isLogin = false;
   api
     .get(`users/logout/${id}`)
     .then(res => {
       console.log(res.data);
     })
     .catch(error => {
-      console.log(error.message);
+      console.log(error.response.data);
     });
 
   AsyncStorage.setItem('token', '').then(token => {
     console.log('logout token: ', token);
   });
-
-
 };
-
-export const getUserInfoByToken = createAsyncThunk(
-  'user/getUserInfoByToken',
-  async (token, { rejectWithValue }) => {
-    try {
-      const response = await api.get(`users/token`, {
-        headers: {
-          Authorization: `Bearer ${token}`,
-          Accept: 'application/json',
-        },
-      });
-      return response.data;
-    } catch (error) {
-      return rejectWithValue(error.message);
-    }
-  },
-);
