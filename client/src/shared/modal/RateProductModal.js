675c8fe95535457c0ebbc4b970d8f875118cbdd8
daryledevs <daryledevs@gmail.com>
2022-12-01 02:28:15 +0800

    Created a submit handler for editing and submiting new review

--- client/src/shared/modal/RateProductModal.js
+++ client/src/shared/modal/RateProductModal.js
@@ -5,16 +5,62 @@ import star_solid from '../../asset/image/star_solid.png';
 import Icon from 'react-native-vector-icons/FontAwesome5';
 import { useSelector } from 'react-redux';
 import { UserDetails } from '../../redux/reducer/User';
+import Icon_2 from 'react-native-vector-icons/MaterialIcons';
+import api from '../../asset/api';
 
-const RateProductModal = ({ isVisible, userStarRating, setShowReviewModal }) => {
+const RateProductModal = ({
+  user_id,
+  isVisible,
+  product_id,
+  userStarRating,
+  submitEditReview,
+  userCurrentReview,
+  setShowReviewModal,
+  setSubmitEditReview,
+}) => {
   const timesToIterate = [1, 2, 3, 4, 5];
   const [starRating, setStarRating] = useState(0);
+  const [changeTextReview, setChangeTextReview] = useState("");
   const userData = useSelector(UserDetails);
 
   useEffect(() => {
-    setStarRating(userStarRating);
+    setStarRating(
+      userCurrentReview.product_rating
+        ? userCurrentReview.product_rating
+        : userStarRating,
+    );
+    setChangeTextReview(userCurrentReview.user_comment);
   }, [isVisible, userStarRating]);
-  
+
+  function SubmitHandler(){
+    if(userCurrentReview.review_id){
+      api
+        .put(`/review/${userCurrentReview.review_id}`, {
+          product_rating: starRating,
+          user_comment: changeTextReview,
+        })
+        .then(() => {
+          setSubmitEditReview(!submitEditReview);
+          setShowReviewModal(false);
+        })
+        .catch(error => {
+          console.log(error.message);
+        });
+    } else {
+      api.post(`/review/${product_id}/${user_id}`, {
+        product_rating: starRating,
+        user_comment: changeTextReview,
+      })
+      .then(() => {
+        setSubmitEditReview(!submitEditReview);
+        setShowReviewModal(false);
+      })
+      .catch((error) => {
+        console.log(error.message);
+      })
+    }
+  };
+
   return (
     <Modal
       useNativeDriver={true}
@@ -26,7 +72,15 @@ const RateProductModal = ({ isVisible, userStarRating, setShowReviewModal }) => 
         <Pressable onPress={() => setShowReviewModal(false)}>
           <Text style={{fontSize: 25}}>X</Text>
         </Pressable>
-        <View style={{flexDirection: 'row', alignItems: 'center'}}>
+        <Text style={{marginVertical: 15, fontWeight: 'bold', fontSize: 18}}>
+          Rate and review
+        </Text>
+        <View
+          style={{
+            flexDirection: 'row',
+            alignItems: 'center',
+            marginBottom: 10,
+          }}>
           <Icon
             name="user-alt"
             color="grey"
@@ -40,6 +94,19 @@ const RateProductModal = ({ isVisible, userStarRating, setShowReviewModal }) => 
             <Text>{userData.email}</Text>
           </View>
         </View>
+        <View
+          style={{
+            flexDirection: 'row',
+            alignItems: 'center',
+            marginBottom: 10,
+          }}>
+          <Text style={{fontSize: 16}}>This will be posted in public</Text>
+          <Icon_2
+            name="error-outline"
+            size={17}
+            style={{marginHorizontal: 2}}
+          />
+        </View>
         <View style={styles.starContainer}>
           {timesToIterate.map(item => {
             return (
@@ -49,14 +116,43 @@ const RateProductModal = ({ isVisible, userStarRating, setShowReviewModal }) => 
                   setStarRating(item);
                 }}>
                 <Image
-                  source={starRating >= item ? star_solid : star_plain}
+                  source={
+                    userCurrentReview.product_rating
+                      ? userCurrentReview.product_rating >= item
+                        ? star_solid
+                        : star_plain
+                      : starRating >= item
+                      ? star_solid
+                      : star_plain
+                  }
                   style={{width: 35, height: 35}}
                 />
               </Pressable>
             );
           })}
         </View>
-        <TextInput placeholder="Write comment" style={styles.comment} />
+        <TextInput
+          placeholder="Write comment"
+          style={styles.comment}
+          value={changeTextReview}
+          onChangeText={text => setChangeTextReview(text)}
+        />
+        <Pressable
+          onPress={SubmitHandler}
+          style={{
+            alignSelf: 'center',
+            marginTop: 20,
+            padding: 10,
+            width: 100,
+            justifyContent: 'center',
+            alignItems: 'center',
+            borderRadius: 30,
+            backgroundColor: '#6BCB77',
+          }}>
+          <Text style={{fontSize: 18, color: 'white', fontWeight: 'bold'}}>
+            Submit
+          </Text>
+        </Pressable>
       </View>
     </Modal>
   );
@@ -76,6 +172,7 @@ const styles = StyleSheet.create({
   starContainer: {
     flexDirection: 'row',
     justifyContent: 'space-between',
+    marginBottom: 20
   },
 
   comment:{
