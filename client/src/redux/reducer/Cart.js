853c9f7389590cd85c956dbadf8b596df470d902
daryledevs <daryledevs@gmail.com>
2022-12-02 21:27:12 +0800

    Created a function adjustQuantity and extra reducers for getAllUserCart

--- client/src/redux/reducer/Cart.js
+++ client/src/redux/reducer/Cart.js
@@ -1,11 +1,15 @@
 import { createSlice } from "@reduxjs/toolkit";
+import api from "../../asset/api";
 import {
   addToCartAction,
   removeFromCartAction,
-  clearCartAction,
+  getAllUserCart,
 } from '../action/Cart';
 
-const initialState = []
+const initialState = {
+  item_cart: [],
+  doneGetWishlist: false,
+};
 
 
 const cartSlice = createSlice({
@@ -14,10 +18,47 @@ const cartSlice = createSlice({
   reducers: {
     addToCart: addToCartAction,
     removeFromCart: removeFromCartAction,
-    clearCart: clearCartAction,
+    adjustQuantity: (state, actions) => {
+      const { isIncrease, product_id, itemQuantity, user_id } = actions.payload;
+      let itemCart = state.item_cart?.find(item => item.product_id === product_id);
+
+      if(isIncrease) itemCart.quantity += itemQuantity;
+      else itemCart.quantity -= itemQuantity;
+
+       api
+         .put(`/cart/${product_id}/${user_id}`, {quantity: itemCart.quantity})
+         .then(() => {
+           console.log('Product quantity updated');
+         });
+    }
   },
+  extraReducers(builder){
+    builder.addCase(getAllUserCart.pending, (state, actions) => {
+      state.doneGetWishlist = false;
+    });
+
+    builder.addCase(getAllUserCart.fulfilled, (state, actions) => {
+      const cartData  = actions.payload.carts;
+      let sort = [];
+      
+      for (let i = 0; i < cartData.length; i++) {
+        sort.push({
+          cart_id: cartData[i]._id,
+          product_id: cartData[i].product_id._id,
+          name: cartData[i].product_id.name,
+          price: cartData[i].product_id.price,
+          quantity: cartData[i].quantity,
+          quantity_stock: cartData[i].product_id.quantity_stock,
+        });
+      };
+
+      state.doneGetWishlist = true;
+      state.item_cart = [...sort];
+    });
+  }
 });
 
-export const { addToCart, removeFromCart, clearCart } = cartSlice.actions;
-export const cartData = (state) => state.cart;
+export const { addToCart, removeFromCart, adjustQuantity } = cartSlice.actions;
+export const cartData = (state) => state.cart.item_cart;
+export const FetchCartStatus = (state) => state.cart.doneGetWishlist;
 export default cartSlice.reducer;
