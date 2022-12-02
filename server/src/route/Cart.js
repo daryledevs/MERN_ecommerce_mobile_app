f0de97dacb242a4dadc5d5b67ce0b818ac085c25
daryledevs <daryledevs@gmail.com>
2022-12-02 21:22:06 +0800

    Added the controller to new route for update

--- server/src/route/Cart.js
+++ server/src/route/Cart.js
@@ -1,9 +1,15 @@
 const express = require("express");
-const { getUserCart, addToCart, removeCart } = require("../controller/Cart");
+const {
+  getUserCart,
+  addToCart,
+  removeCart,
+  productCartQuantity,
+} = require("../controller/Cart");
 const router = express.Router();
 
 router.get("/:user_id", getUserCart);
 router.post("/:product_id/:user_id", addToCart);
-router.delete("/:cart_id", removeCart);
+router.put("/:product_id/:user_id", productCartQuantity);
+router.delete("/:product_id/:user_id", removeCart);
 
 module.exports = router;
