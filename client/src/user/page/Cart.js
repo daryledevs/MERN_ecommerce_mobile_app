7f49b71aa5aca2bac0c6b0187d3e47d8b5b2f1e1
daryledevs <daryledevs@gmail.com>
2022-12-02 21:28:20 +0800

    Added a button for adjusting the product cart's quantity

--- client/src/user/page/Cart.js
+++ client/src/user/page/Cart.js
@@ -4,21 +4,26 @@ import {
   View,
   Image,
   Button,
+  Pressable,
 } from 'react-native';
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import Icons from 'react-native-vector-icons/FontAwesome5';
 import { useDispatch, useSelector } from 'react-redux';
 import { useNavigation } from '@react-navigation/native';
 import { SwipeListView } from 'react-native-swipe-list-view';
 import { TouchableOpacity } from 'react-native-gesture-handler';
-import { clearCart, removeFromCart } from '../../redux/reducer/Cart';
+import { removeFromCart, adjustQuantity } from '../../redux/reducer/Cart';
 import placeholder from '../../asset/image/placeholder.png';
 import Header from '../../shared/others/Header';
+import { UserDetails } from '../../redux/reducer/User';
+import Icon from 'react-native-vector-icons/FontAwesome';
 
 const Cart = () => {
-  const cartData = useSelector(state => state.cart);
+  const cartData = useSelector(state => state.cart.item_cart);
   const dispatch = useDispatch();
   const navigation = useNavigation();
+  const userData = useSelector(UserDetails);
+  const [itemQuantity, setItemQuantity] = useState(1);
   var total = 0;
   cartData.forEach(element => {
     total += element.price * element.quantity;
@@ -28,7 +33,7 @@ const Cart = () => {
     <>
       <Header />
       {cartData.length !== 0 ? (
-        <View style={{height: '100%', position: 'relative'}}>
+        <View style={{flex: 1, position: 'relative'}}>
           <SwipeListView
             previewOpenDelay={3000}
             friction={1000}
@@ -36,15 +41,99 @@ const Cart = () => {
             rightOpenValue={-80}
             disableRightSwipe={true}
             data={cartData}
-            keyExtractor={item => item._id}
+            keyExtractor={item => item.product_id}
             renderItem={(cart, rowMap) => {
               return (
                 <View style={styles.cartContainer}>
                   <Image style={{width: 45, height: 45}} source={placeholder} />
                   <Text style={styles.name}>{cart.item.name}</Text>
-                  <Text style={styles.price}>
-                    {cart.item.quantity ? `${cart.item.quantity}x` : null}
-                  </Text>
+                  <View style={{marginLeft: 'auto'}}>
+                    <View
+                      style={{
+                        flexDirection: 'row',
+                        backgroundColor: 'white',
+                      }}>
+                      <Pressable
+                        onPress={() => {
+                          if (cart.item.quantity > 1) {
+                            dispatch(
+                              adjustQuantity({
+                                isIncrease: false,
+                                product_id: cart.item.product_id,
+                                itemQuantity: 1,
+                                user_id: userData._id,
+                              }),
+                            );
+                          } else {
+                            console.log('LOWEST NUMBER');
+                            dispatch(
+                              removeFromCart({
+                                product_id: cart.item.product_id,
+                                user_id: userData._id,
+                              }),
+                            );
+                            // prompt modal or error here
+                          }
+                        }}
+                        style={{
+                          borderWidth: 1,
+                          borderColor: '#ccc',
+                          padding: 5,
+                          alignSelf: 'center',
+                        }}>
+                        <Icon
+                          name="minus"
+                          size={20}
+                          style={{marginHorizontal: 5}}
+                        />
+                      </Pressable>
+                      <View
+                        style={{
+                          flexDirection: 'column',
+                          borderWidth: 1,
+                          borderColor: '#ccc',
+                          alignItems: 'center',
+                        }}>
+                        <Text
+                          style={{
+                            fontSize: 20,
+                            width: 40,
+                            textAlign: 'center',
+                            alignSelf: 'center',
+                          }}>
+                          {cart.item.quantity ? `${cart.item.quantity}x` : null}
+                        </Text>
+                      </View>
+                      <Pressable
+                        onPress={() => {
+                          if (cart.item.quantity < cart.item.quantity_stock) {
+                            dispatch(
+                              adjustQuantity({
+                                isIncrease: true,
+                                product_id: cart.item.product_id,
+                                itemQuantity: 1,
+                                user_id: userData._id,
+                              }),
+                            );
+                          } else {
+                            console.log('MAXIMUM NUMBER');
+                            // prompt modal or error here
+                          }
+                        }}
+                        style={{
+                          borderWidth: 1,
+                          borderColor: '#ccc',
+                          padding: 5,
+                          alignSelf: 'center',
+                        }}>
+                        <Icon
+                          name="plus"
+                          size={20}
+                          style={{marginHorizontal: 5}}
+                        />
+                      </Pressable>
+                    </View>
+                  </View>
                   <Text style={styles.price}>
                     ₱{cart.item.quantity * cart.item.price}
                   </Text>
@@ -55,7 +144,14 @@ const Cart = () => {
               <View style={styles.hiddenContainer}>
                 <TouchableOpacity
                   style={styles.hiddenButton}
-                  onPress={() => dispatch(removeFromCart(data.item._id.$oid))}>
+                  onPress={() =>
+                    dispatch(
+                      removeFromCart({
+                        product_id: data.item.product_id,
+                        user_id: userData._id,
+                      }),
+                    )
+                  }>
                   <Icons name="trash" color={'white'} size={30} />
                 </TouchableOpacity>
               </View>
@@ -64,7 +160,6 @@ const Cart = () => {
 
           <View style={styles.operationsContainer}>
             <Text style={styles.totalPrice}>₱{total}</Text>
-            <Button title="Clear" onPress={() => dispatch(clearCart())} />
             <Button
               title="Checkout"
               onPress={() => navigation.navigate('Cart Checkout')}
