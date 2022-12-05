b02c5d15c2f7bd7b414bc2ea76e59248da28b381
daryledevs <daryledevs@gmail.com>
2022-12-05 22:55:27 +0800

    Created a address form modal and customInput for the form

---
+++ client/src/shared/modal/AddressFillUpModal.js
@@ -0,0 +1,164 @@
+import { StyleSheet, Text, View, Modal, Pressable, ScrollView } from 'react-native';
+import React from 'react';
+import { Formik, Field } from 'formik';
+import * as yup from 'yup';
+import CustomInput from '../others/CustomInput';
+import Icon from "react-native-vector-icons/AntDesign";
+import api from '../../asset/api';
+import { useDispatch, useSelector } from 'react-redux';
+import { UserDetails, addNewAddress } from '../../redux/reducer/User';
+import { useState } from 'react';
+import LoadingScreen from '../loading/LoadingScreen';
+
+const AddressFillUpModal = ({showModal, closeModal}) => {
+
+  const user_details = useSelector(UserDetails);
+  const dispatch = useDispatch();
+  const [isLoading, setIsLoading] = useState(false);
+
+  const signUpValidationSchema = yup.object().shape({
+    house_number: yup.number().required('House number is required'),
+    zip_code: yup.number().required('ZIP is required'),
+    street: yup.string().required('Street is required'),
+    subdivision: yup.string().required('Subdivision is required'),
+    barangay: yup.string().required('Barangay is required'),
+    city: yup.string().required('City is required'),
+    province: yup.string().required('Province is required'),
+  });
+
+  function SubmitHandler(values) {
+    setIsLoading(true);
+    api
+      .post(`/user-address/${user_details._id}`, {
+        ...values,
+      })
+      .then(res => {
+        dispatch(addNewAddress(res.data.user_address));
+        closeModal();
+        setIsLoading(false);
+      })
+      .catch(error => {
+        console.log(error.response.data);
+      });
+  }
+
+  return (
+    <Modal
+      animationType="fade"
+      transparent={true}
+      visible={showModal}
+      onRequestClose={() => closeModal()}>
+      <ScrollView contentContainerStyle={styles.modalContainer}>
+        <LoadingScreen isVisible={isLoading} />
+        <View style={styles.fillUpFormContainer}>
+          <View
+            style={{
+              flexDirection: 'row',
+              alignItems: "center"
+            }}>
+            <Text style={{fontWeight: 'bold', fontSize: 18, margin: 5}}>
+              Present Address
+            </Text>
+            <Icon
+              name="close"
+              size={25}
+              style={{marginLeft: 'auto'}}
+              onPress={closeModal}
+            />
+          </View>
+          <Formik
+            validationSchema={signUpValidationSchema}
+            initialValues={{
+              house_number: '',
+              street: '',
+              subdivision: '',
+              city: '',
+              barangay: '',
+              province: '',
+            }}
+            onSubmit={values => SubmitHandler(values)}>
+            {({handleSubmit, isValid}) => (
+              <>
+                <Field
+                  component={CustomInput}
+                  name="house_number"
+                  placeholder="House Number"
+                  keyboardType="numeric"
+                />
+
+                <Field
+                  component={CustomInput}
+                  name="street"
+                  placeholder="Street"
+                />
+
+                <Field
+                  component={CustomInput}
+                  name="subdivision"
+                  placeholder="Subdivision"
+                />
+
+                <Field component={CustomInput} name="city" placeholder="City" />
+
+                <Field
+                  component={CustomInput}
+                  name="barangay"
+                  placeholder="Barangay"
+                  keyboardType="numeric"
+                />
+
+                <Field
+                  component={CustomInput}
+                  name="province"
+                  placeholder="Province"
+                />
+
+                <Field
+                  component={CustomInput}
+                  name="zip_code"
+                  placeholder="ZIP"
+                  keyboardType="numeric"
+                />
+
+                <Pressable
+                  onPress={handleSubmit}
+                  disabled={!isValid}
+                  style={{
+                    borderWidth: 1,
+                    borderColor: '#ccc',
+                    padding: 5,
+                    width: '40%',
+                    backgroundColor: 'white',
+                    alignSelf: 'center',
+                    borderRadius: 100,
+                    marginVertical: 10,
+                    alignItems: 'center',
+                  }}>
+                  <Text style={{fontSize: 18, fontWeight: 'bold'}}>ADD</Text>
+                </Pressable>
+              </>
+            )}
+          </Formik>
+        </View>
+      </ScrollView>
+    </Modal>
+  );
+};
+
+export default AddressFillUpModal
+
+const styles = StyleSheet.create({
+  modalContainer: {
+    flex: 1,
+    backgroundColor: 'rgba(0, 0, 0, 0.5)',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+
+  fillUpFormContainer: {
+    backgroundColor: 'white',
+    width: '80%',
+    padding: 15,
+    borderRadius: 10
+  },
+});
---
+++ client/src/shared/others/CustomInput.js
@@ -0,0 +1,55 @@
+import React from 'react';
+import {Text, TextInput, StyleSheet, View} from 'react-native';
+
+const CustomInput = props => {
+  const {
+    field: {name, onBlur, onChange, value},
+    form: {errors, touched, setFieldTouched},
+    ...inputProps
+  } = props;
+
+  const hasError = errors[name] && touched[name];
+
+  return (
+    <View style={{margin: 10 }}>
+      <TextInput
+        style={[
+          styles.textInput,
+          props.multiline && {
+            height: props.numberOfLines * 40,
+            textAlignVertical: 'top',
+          },
+          hasError && styles.errorInput,
+        ]}
+        value={value}
+        onChangeText={text => onChange(name)(text)}
+        onBlur={() => {
+          setFieldTouched(name);
+          onBlur(name);
+        }}
+        {...inputProps}
+      />
+      <Text style={styles.errorText}>{hasError && errors[name]}</Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  textInput: {
+    height: 40,
+    backgroundColor: 'white',
+    borderColor: 'black',
+    borderWidth: 1,
+    borderColor: '#ccc',
+    borderRadius: 10,
+  },
+  errorText: {
+    fontSize: 10,
+    color: 'red',
+  },
+  errorInput: {
+    borderColor: 'red',
+  },
+});
+
+export default CustomInput;
