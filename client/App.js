2da0a9fa0e3ba0d38463cfc24b2ff344244c4856
daryledevs <daryledevs@gmail.com>
2022-12-07 23:43:58 +0800

    Fixed the bug that bottom tab jumping on first load

--- client/App.js
+++ client/App.js
@@ -1,17 +1,20 @@
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet } from 'react-native';
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 // component
 import MainNavigation from './src/navigation/MainNavigation';
 import { Provider } from 'react-redux';
 import store from './src/redux/store';
+import { SafeAreaProvider } from 'react-native-safe-area-context';
 
 const App = () => {
   return (
     <Provider store={store}>
-      <NavigationContainer>
-        <MainNavigation />
-      </NavigationContainer>
+      <SafeAreaProvider>
+        <NavigationContainer>
+          <MainNavigation />
+        </NavigationContainer>
+      </SafeAreaProvider>
     </Provider>
   );
 };
