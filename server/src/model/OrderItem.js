15bb9280163bace712e322de1191c1520f218e47
daryledevs <daryledevs@gmail.com>
2022-12-17 05:25:26 +0800

    Created a new model for order items

---
+++ server/src/model/OrderItem.js
@@ -0,0 +1,16 @@
+const mongoose = require("mongoose");
+
+const orderItemSchema = mongoose.Schema({
+  product:{
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "Product"
+  },
+
+  quantity:{
+    type: Number,
+    default: null,
+  }
+});
+
+
+exports.OrderItem = mongoose.model("OrderItem", orderItemSchema);
