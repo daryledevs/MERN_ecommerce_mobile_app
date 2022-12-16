038fff69286452cb38f6a74367fa2c5115754e85
daryledevs <daryledevs@gmail.com>
2022-12-17 05:34:51 +0800

    Added order and stripe to our routes

--- server/index.js
+++ server/index.js
@@ -5,7 +5,6 @@ const { default: mongoose } = require("mongoose");
 require("dotenv/config");
 
 const app = express();
-
 app.use(bodyParser.json());
 app.use(morgan("tiny"));
 
@@ -18,12 +17,15 @@ const user = require("./src/route/User");
 const like = require("./src/route/Wishlist");
 const review = require("./src/route/Review");
 const cart = require("./src/route/Cart");
+const order = require("./src/route/Order");
 const address = require("./src/route/UserAddress");
+const checkout = require("./src/route/Checkout");
 
 // for testing
 app.get(`${API}/hello`, (req, res) => {
   res.send("Hello world");
 });
+
 
 // routes
 app.use(`${API}/products`, product);
@@ -32,7 +34,9 @@ app.use(`${API}/users`, user);
 app.use(`${API}/likes`, like);
 app.use(`${API}/review`, review);
 app.use(`${API}/cart`, cart);
+app.use(`${API}/order`, order);
 app.use(`${API}/user-address`, address);
+app.use(`${API}/stripe`, checkout);
 
 // database
 mongoose.connect(process.env.CONNECTION_URL, {
