41d95d4841146c593c06fa74f3316a0a4eee894e
daryledevs <daryledevs@gmail.com>
2022-12-21 05:44:10 +0800

    Modified GetCurrentPage to have an option to on or off the bottom tab navigation

--- client/src/util/GetCurrentPage.js
+++ client/src/util/GetCurrentPage.js
@@ -3,16 +3,16 @@ import { useCallback } from 'react';
 import { useDispatch } from 'react-redux';
 import { get_current_page } from '../redux/reducer/RouteNavigation';
 
-const GetCurrentPage = (status) => {
+const GetCurrentPage = ({ on, off }) => {
   const dispatch = useDispatch();
 
   useFocusEffect(
     useCallback(() => {
-      dispatch(get_current_page(status));
+      dispatch(get_current_page(on));
       return () => {
-        dispatch(get_current_page(!status));
+        dispatch(get_current_page(off));
       };
-    }, []),
+    }, [off]),
   );
 };
 
