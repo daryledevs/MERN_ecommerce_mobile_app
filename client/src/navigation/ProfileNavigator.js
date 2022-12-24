9f1039d65e0836e1528da35e76c9902f09d0d37d
daryledevs <daryledevs@gmail.com>
2022-12-24 12:56:30 +0800

    Created a new navigator for user page where all the user can view and edit their product review, address, information, and order history

---
+++ client/src/navigation/ProfileNavigator.js
@@ -0,0 +1,52 @@
+import React from 'react';
+import {createStackNavigator} from '@react-navigation/stack';
+// components
+import UserProfile from '../user/page/UserProfile';
+import Shipping from '../user/component/Shipping';
+import Orders from '../user/page/Orders';
+import UserDetail from '../user/component/UserDetail';
+import ResetHistory from '../util/ResetHistory';
+
+const Stack = createStackNavigator();
+
+const ProfileNavigator = () => {
+  ResetHistory('UserProfile');
+
+  return (
+    <Stack.Navigator>
+      <Stack.Screen
+        name="UserProfile"
+        component={UserProfile}
+      />
+
+      <Stack.Screen
+        name="UserDetail"
+        options={{
+          headerShown: true,
+          title: "Edit Profile"
+        }}
+        component={UserDetail}
+      />
+
+      <Stack.Screen
+        name="Shipping"
+        options={{
+          headerShown: true,
+          title: 'Shopping Address',
+        }}
+        component={Shipping}
+      />
+
+      <Stack.Screen
+        name="Order"
+        options={{
+          headerShown: true,
+          title: 'My Purchases',
+        }}
+        component={Orders}
+      />
+    </Stack.Navigator>
+  );
+};
+
+export default ProfileNavigator;
