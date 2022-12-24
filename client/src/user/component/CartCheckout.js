15a6bfe75064d3c902a83e94ee64a14c87866d52
daryledevs <daryledevs@gmail.com>
2022-12-24 12:30:05 +0800

    Return to cart page after clicking okay on success modal, item product from cart checkout is viewable, user can choose where to ship any from their saved list of address

--- client/src/user/component/CartCheckout.js
+++ client/src/user/component/CartCheckout.js
@@ -1,4 +1,8 @@
-import {StyleSheet, Text, View, ScrollView, TouchableOpacity, Alert} from 'react-native';
+import {StyleSheet, Text, View, ScrollView, TouchableOpacity, Alert, Image} from 'react-native';
+import {
+  CommonActions,
+  useNavigation,
+} from '@react-navigation/native';
 import React, { useState} from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { UserAddress, UserDetails } from '../../redux/reducer/User';
@@ -7,22 +11,29 @@ import {StripeProvider, useStripe, presentPaymentSheet, initPaymentSheet} from '
 import { useEffect } from 'react';
 import getPublishableKey from '../../util/PublishableKey';
 import { create_orders } from '../../redux/action/Order';
+import { FlatList } from 'react-native-gesture-handler';
+import placeholder from '../../asset/image/placeholder.png';
+import Icon from 'react-native-vector-icons/AntDesign';
 
 const CartCheckout = () => {
+  let total = 0;
+  const navigation = useNavigation();
   const dispatch = useDispatch();
   const cartData = useSelector(state => state.cart.item_cart);
   const user_details = useSelector(UserDetails);
   const user_address = useSelector(UserAddress);
+  const [defaultAddress, setDefaultAddress] = useState(user_details.address_id);
   const [fetchPublishableKey, setFetchPublishableKey] = useState(null);
   const [fetchPubKeyDone, setFetchPubKeyDone] = useState(true);
   const [loading, setLoading] = useState(false);
-
-  var total = 0;
+  console.log('CART DATA: ', cartData);
   cartData.forEach(element => {
-    total += element.price * element.quantity;
+    total += element.product_id.price * element.quantity;
   });
+  
 
   useEffect(() => {
+
     async function initialized() {
       const publishableKey = await getPublishableKey();
       if (!publishableKey)
@@ -34,7 +45,7 @@ const CartCheckout = () => {
     const initializePaymentSheet = async () => {
       api
         .post('/stripe/checkout', {
-          name: `${user_details.give_name}${user_details.last_name_}`,
+          name: `${user_details.give_name}${user_details.last_name}`,
           paymentMethodType: 'card',
           currency: 'PHP',
           subtotal: total,
@@ -49,11 +60,11 @@ const CartCheckout = () => {
             paymentIntentClientSecret: paymentIntent,
             allowsDelayedPaymentMethods: true,
             defaultBillingDetails: {
-              name: `${user_details.give_name}${user_details.last_name_}`,
+              name: `${user_details.give_name}${user_details.last_name}`,
             },
           });
 
-          if (error) setLoading(true);
+          if (!error) setLoading(true);
         })
         .catch(error => {
           Alert.alert('Error', error.message);
@@ -70,20 +81,120 @@ const CartCheckout = () => {
       Alert.alert(`${error.code}`, error.message);
     } else {
       dispatch(create_orders());
-      Alert.alert('Success', 'Your order is confirmed!');
+      
+      Alert.alert('Success', 'Your order is confirmed!', [
+        {text: 'OK', onPress: () => {
+          navigation.dispatch(state => {
+            return CommonActions.reset({
+              index: 0,
+              routes: [{name: 'Cart Screen'}],
+            });
+          });
+        }},
+      ]);
     }
+  };
+
+  async function ChangeAddress(address){
+    api
+      .put(`/users/edit-profile/${user_details._id}`, {
+        address_id: address,
+      })
+      .then(() => {
+        setDefaultAddress(address);
+        console.log('Changed default address');
+      })
+      .catch(error => {
+        console.log(error.response.data);
+      });
   };
 
   if (!fetchPubKeyDone) return <></>;
   return (
     <StripeProvider publishableKey={`${fetchPublishableKey}`}>
-      <ScrollView
-        nestedScrollEnabled={true}
-        contentContainerStyle={{
-          flex: 1,
-          backgroundColor: 'white',
-        }}>
-        <ScrollView nestedScrollEnabled={true}>
+      <ScrollView>
+        <View>
+          <Text
+            style={{
+              fontWeight: 'bold',
+              fontSize: 17,
+            }}>
+            Shipping to:
+          </Text>
+          <FlatList
+            contentContainerStyle={{flexGrow: 1, justifyContent: 'center'}}
+            style={{
+              padding: 5,
+              marginVertical: 5,
+              borderBottomColor: '#ccc',
+              borderBottomWidth: 1,
+            }}
+            keyboardShouldPersistTaps="handled"
+            showsHorizontalScrollIndicator={false}
+            horizontal
+            data={user_address}
+            keyExtractor={item => `${item._id}_${Date.now()}`}
+            renderItem={({item}) => {
+              return (
+                <TouchableOpacity
+                  onPress={() => ChangeAddress(item._id)}
+                  style={{
+                    height: 120,
+                    width: 360,
+                    justifyContent: 'center',
+                    padding: 15,
+                    borderRadius: 10,
+                    alignItems: 'center',
+                    elevation: 10,
+                    zIndex: 5,
+                    backgroundColor: 'white',
+                    marginVertical: 10,
+                    marginHorizontal: 10,
+                  }}>
+                  <View>
+                    <View style={{flexDirection: 'row'}}>
+                      <Text
+                        style={{
+                          fontWeight: 'bold',
+                          fontSize: 15,
+                        }}>
+                        {user_details.given_name} {user_details.last_name}{' '}
+                      </Text>
+                      <Text>| {user_details.phone}</Text>
+                    </View>
+                    <View style={{flexDirection: 'row'}}>
+                      <Text>
+                        {item.house_number} {item.street} St., Barangay{' '}
+                        {item.barangay}, {item.subdivision}, {item.city},{' '}
+                        {item.province}, {item.zip_code}
+                      </Text>
+                    </View>
+                  </View>
+                  {defaultAddress === item?._id ? (
+                    <View
+                      style={{
+                        marginTop: 'auto',
+                        marginLeft: 'auto',
+                        flexDirection: 'row',
+                        alignItems: 'center',
+                      }}>
+                      <Text style={{marginRight: 5, fontSize: 14}}>
+                        Delivery Address
+                      </Text>
+                      <Icon
+                        name="checkcircleo"
+                        size={19}
+                        color="white"
+                        style={{backgroundColor: 'blue', borderRadius: 100}}
+                      />
+                    </View>
+                  ) : null}
+                </TouchableOpacity>
+              );
+            }}
+          />
+        </View>
+        <View>
           <Text
             style={{
               fontWeight: 'bold',
@@ -94,57 +205,71 @@ const CartCheckout = () => {
           </Text>
           {cartData.map(item => {
             return (
-              <View key={item.cart_id} style={styles.order_cards}>
-                <Text>{item.name}</Text>
-                <Text>₱{item.price * item.quantity}</Text>
-                <Text>{item.quantity}</Text>
-              </View>
+              <TouchableOpacity
+                onPress={() => {
+                  navigation.navigate('Product Detail', {
+                    product: item.product_id,
+                  });
+                }}
+                key={`${item._id}__${Date.now()}`}
+                style={styles.order_cards}>
+                <Image
+                  style={{
+                    height: 100,
+                    width: 100,
+                  }}
+                  source={
+                    item.product_id.image
+                      ? {uri: item.product_id.image}
+                      : placeholder
+                  }
+                />
+                <View>
+                  <Text>{item.product_id.name}</Text>
+                  <Text>₱{item.product_id.price * item.quantity}</Text>
+                  <Text>QTY: {item.quantity}</Text>
+                </View>
+              </TouchableOpacity>
             );
           })}
-        </ScrollView>
+        </View>
+      </ScrollView>
+      <View
+        style={{
+          borderTopWidth: 1,
+          borderColor: '#ccc',
+          flexDirection: 'row',
+          justifyContent: 'space-evenly',
+          backgroundColor: 'white',
+        }}>
         <View
           style={{
-            flex: 3,
-            justifyContent: 'space-evenly',
-            padding: 5,
+            flex: 1,
+            justifyContent: 'center',
+            alignItems: 'center',
+            flexDirection: 'row',
+            flexGrow: 1,
+            flexShrink: 0,
           }}>
-          <View>
-            <Text
-              style={{
-                fontWeight: 'bold',
-                fontSize: 17,
-              }}>
-              Shipping to:
-            </Text>
-            <Text
-              style={{
-                color: 'black',
-              }}>
-              {user_address[0]?.house_number} {user_address[0]?.street} St.,
-              Barangay {user_address[0]?.barangay},{' '}
-              {user_address[0]?.subdivision}, {user_address[0]?.city},{' '}
-              {user_address[0]?.province}, {user_address[0]?.zip_code}
-            </Text>
-          </View>
-
-          <View>
-            <Text
-              style={{
-                fontWeight: 'bold',
-                fontSize: 17,
-              }}>
-              Order Info:
-            </Text>
-            <Text
-              style={{
-                color: 'black',
-              }}>
-              Subtotal: {total}
-            </Text>
-          </View>
+          <Text
+            style={{
+              color: 'black',
+              marginRight: 5,
+              fontSize: 17,
+              fontWeight: 'bold',
+            }}>
+            Subtotal:
+          </Text>
+          <Text
+            style={{
+              color: 'black',
+              fontSize: 17,
+            }}>
+            ₱{total}
+          </Text>
         </View>
         <TouchableOpacity
-          disabled={loading}
+          disabled={!loading}
           onPress={openPaymentSheet}
           style={{
             alignSelf: 'center',
@@ -164,7 +289,7 @@ const CartCheckout = () => {
             Checkout
           </Text>
         </TouchableOpacity>
-      </ScrollView>
+      </View>
     </StripeProvider>
   );
 };
@@ -173,11 +298,17 @@ export default CartCheckout;
 
 const styles = StyleSheet.create({
   order_cards:{
-    borderBottomWidth: 1,
     borderColor: "#ccc",
     flexDirection: 'row',
-    justifyContent: "space-between",
+    alignItems: "center",
     padding: 5,
-    paddingLeft: 20
+    borderWidth: 1,
+    width: "80%",
+    borderRadius: 10,
+    backgroundColor: "white",
+    elevation: 5,
+    alignSelf: "center",
+    justifyContent: "space-evenly",
+    marginBottom: 10
   }
 });
